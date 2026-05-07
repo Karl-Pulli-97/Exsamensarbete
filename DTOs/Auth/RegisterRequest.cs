4c75db8f9154785457e5e9abb9e7a31683579a7e
Karl Pulli <kalle.pulli@hotmail.com>
2026-05-07 18:57:44 +0200

    JWT setup + RegisterRequest DTO + Refactoring

---
+++ DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace FishingLog.Api.DTOs.Auth;
+
+public class RegisterRequest
+{
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Name { get; set; } = null!;
+}
