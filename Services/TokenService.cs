c38d8db097752476847bfcfb42412edad95db40b
Karl Pulli <kalle.pulli@hotmail.com>
2026-05-07 19:09:29 +0200

    TokenService

---
+++ Services/TokenService.cs
@@ -0,0 +1,47 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FishingLog.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FishingLog.Api.Services;
+
+public class TokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var jwtSettings = _configuration.GetSection("Jwt");
+        var key = jwtSettings["Key"]!;
+        var issuer = jwtSettings["Issuer"];
+        var audience = jwtSettings["Audience"];
+        var tokenTimeout = int.Parse(jwtSettings["TokenTimeout"]!);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim("name", user.Name),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(tokenTimeout),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
