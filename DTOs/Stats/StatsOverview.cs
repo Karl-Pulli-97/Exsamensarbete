9a31697c52755a73bfbbdf00d287d46dc3e17a12
Karl Pulli <kalle.pulli@hotmail.com>
2026-05-12 14:04:14 +0200

    StatsOverviewDto

---
+++ DTOs/Stats/StatsOverview.cs
@@ -0,0 +1,12 @@
+namespace FishingLog.Api.DTOs.Stats;
+
+public class StatsOverviewDto
+{
+    public int TotalCatches { get; set; }
+    public int ReleasedCatches { get; set; }
+    public decimal? LargestWeight { get; set; }
+    public decimal? LargestLength { get; set; }
+    public string? MostCaughtSpecies { get; set; }
+    public string? BestLure { get; set; }
+    public string? BestLocation { get; set; }
+}
