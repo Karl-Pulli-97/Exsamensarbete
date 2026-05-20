82c0214c1140ec0c576e8ebe289feee15103ef14
Karl Pulli <kalle.pulli@hotmail.com>
2026-05-21 01:46:37 +0200

    Map name change + Update unused import

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Logo } from './Logo';
 import { UserMenu } from './UserMenu';
 
